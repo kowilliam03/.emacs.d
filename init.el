3f796c0b67e2cef0dcfc6e1a48c5d249e707a331
kwn <kowilliam03@gmail.com>
2021-02-19 15:29:34 +0800

    modify some UI setting

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 
 (scroll-bar-mode -1)    ; Disable visible scrollbar
 (tool-bar-mode -1)      ; Disable the toolbar
+
 (tooltip-mode -1)       ; Disable tooltips
 (set-fringe-mode 10)    ; Give some breathing room
 
@@ -13,18 +14,15 @@
 
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 135)
 
-;; Theme 
-;;(load-theme 'wombat)
-(use-package doom-themes
-  :ensure t
-  :defer t
-  :config
-  (setq doom-themes-enable-bold t)
-  (setq doom-themes-enable-italic t)
-  (doom-themes-visual-bell-config)
-  (doom-themes-org-config)
-  )
-(load-theme 'doom-nord t)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -47,13 +45,32 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Theme 
+;;(load-theme 'wombat)
+(use-package doom-themes
+  :defer t
+  :config
+  (setq doom-themes-enable-bold t)
+  (setq doom-themes-enable-italic t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config)
+  )
+
+(load-theme 'doom-gruvbox-light t)
+
 (use-package command-log-mode)
 
 (use-package counsel
-  :ensure t)
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil) ;; Don't start searches with ^
+  )
 
 (use-package ivy
-  :ensure t
   :diminish
   :bind (("C-s" . swiper)
 	 :map ivy-minibuffer-map
@@ -75,12 +92,9 @@
   )
 
 (use-package swiper
-  :ensure t
   :bind (("C-s" . swiper)
 	 ("C-r" . swiper)
-	 ("C-c C-r" . ivy-resume)
-	 ("M-x" . counsel-M-x)
-	 ("C-x C-f" . counsel-find-file))
+	 ("C-c C-r" . ivy-resume))
   :config
   (progn
     (ivy-mode 1)
@@ -90,19 +104,48 @@
   )
 
 (use-package doom-modeline
-  :ensure t
   :init (doom-modeline-mode 1)
   :custom
   ((doom-modeline-height 20))
   )
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode)
+  )
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.4)
+  )
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1)
+  )
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key)
+  )
+  
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" default))
  '(global-command-log-mode t)
- '(package-selected-packages '(doom-themes counsel ivy command-log-mode use-package)))
+ '(package-selected-packages
+   '(helpful ivy-rich which-key rainbow-delimiters doom-themes counsel ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
