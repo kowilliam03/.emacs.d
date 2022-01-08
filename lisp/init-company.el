dc105e28cc9267a425c93141ce6be8f523ece3da
kwn <kowilliam03@gmail.com>
2022-01-08 15:44:07 +0800

    Add company-mode & evil-mode

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,12 +1,28 @@
 ;; -*- lexical-binding: t; -*-
 
 
-(require 'company)
-
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
 
-(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'prog-mode-hook
+	  #'(lambda ()
+	      (require 'company)
+	      ;; Config for company-mode
+	      (setq-default company-tooltip-align-annotations t
+			    company-tooltip-limit 12
+			    company-idle-delay 0
+			    company-echo-delay nil
+			    company-minimum-prefix-length 1
+			    company-require-match nil
+			    company-dabbrev-ignore-case nil
+			    company-dabbrev-downcase nil
+			    company-dabberv-other-buffers 'all
+			    )
+	      ;; Enable Company-mode
+	      (global-company-mode))
+
+	  )
+
 (with-eval-after-load 'company
   (define-key company-mode-map (kbd "M-/") 'company-complete)
   (define-key company-mode-map [remap completion-at-point] 'company-complete)
@@ -16,16 +32,7 @@
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
   (define-key company-active-map (kbd "M-.") 'company-show-location)
-  (setq-default company-tooltip-align-annotations t
-		company-tooltip-limit 12
-		company-idle-delay 0
-		company-echo-delay nil
-		company-minimum-prefix-length 1
-		company-require-match nil
-		company-dabbrev-ignore-case nil
-		company-dabbrev-downcase nil
-		company-dabberv-other-buffers 'all))
-(global-set-key (kbd "C-M-i") 'company-complete)
-
+  (global-set-key (kbd "C-M-i") 'company-complete))
+	  
 
 (provide 'init-company)
--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,12 +1,15 @@
 ;; -*- lexical-binding: t -*-
 
-(setq evil-want-integration t
-      evil-want-keybinding nil
-      evil-want-C-u-scroll t
-      evil-want-C-i-jump nil)
 
-(require 'evil)
-;;(add-hook 'after-init-hook 'evil)
+(add-hook 'after-init-hook
+	  #'(lambda ()
+	      (require 'evil)
+	      (setq evil-want-integration t
+		    evil-want-keybinding nil
+		    evil-want-C-u-scroll t
+		    evil-want-C-i-jump nil)
+	      (evil-mode 1)))
+
 (with-eval-after-load 'evil
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
@@ -15,8 +18,11 @@
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
+
   (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
+  (evil-set-initial-state 'dashboard-mode 'normal)
+  (evil-set-initial-state 'dired-mode 'emacs)
+  )
 
 (global-set-key (kbd "<escape>") 'keyborad-escape-quit)
 
