aa6a53c05aa06a4ce14348d38b6ad5450467fd81
kwn <kowilliam03@gmail.com>
2022-01-19 12:34:12 +0800

    use autoload to load paredit

--- config/init-lisp.el
+++ config/init-lisp.el
@@ -1,8 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook
 	  #'(lambda ()
 	      (enable-paredit-mode)))
-
 
 (provide 'init-lisp)
