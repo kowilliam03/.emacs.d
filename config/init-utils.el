2d1e517406bae17050c2398cf4ba6d243bde26da
kwn <kowilliam03@gmail.com>
2022-01-23 20:50:22 +0800

    add init-config-file func

--- config/init-utils.el
+++ config/init-utils.el
@@ -14,4 +14,20 @@
 ;; Toggle Fullscreem mode
 (define-key global-map (kbd "<f11>") #'toggle-frame-fullscreen)
 
+
+(defun kwn/init-config-file ()
+"enable lexical-binding & disable code-byte compile"
+(interactive)
+(let (file-name)
+  (setq file-name (car (split-string (buffer-name) "\\.")))
+  (progn
+    (goto-char (point-min))
+    (insert (format ";;; %s.el -*- lexical-binding: t no-byte-compile: t -*-" file-name))
+    (insert "\n\n\n\n")
+    (goto-char (point-max))
+    (insert (format "(provide '%s)" file-name))
+    (forward-line -2)
+    ))
+)
+
 (provide 'init-utils)
