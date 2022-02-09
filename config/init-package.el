2bec74b4fb722d80aba2d7eb5ff5195197c0acc2
kwn <kowilliam03@gmail.com>
2022-02-09 17:56:10 +0800

    temp

--- config/init-package.el
+++ config/init-package.el
@@ -1,9 +1,27 @@
 ;;; init-package.el -*- lexical-binding: t no-byte-compile: t -*-
-;; Install package from Github
+
+(autoload 'magit-submodule-add "magit" "" t)
+(autoload 'magit-submodule-remove "magit" "" t)
 
 (defun kwn/compile-package ()
   (interactive)
   (native-compile-async "~/.emacs.d/packages" 'recursively))
+
+(defun kwn/install-package ()
+  (interactive)
+  (setq url (read-string "Git Repository's URL: "))
+  (let ((pkg-dir "packages/"))
+    (magit-submodule-add
+     url
+     (concat pkg-dir (file-name-base url))
+     (file-name-base url)
+     ))
+  )
+
+(defun kwn/remove-package ()
+  (interactive)
+  (magit-submodule-remove (list (magit-read-module-path "Remove module")) "--force" nil)
+  )
 
 
 (provide 'init-package)
