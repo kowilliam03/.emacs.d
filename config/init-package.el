ee0ab9a621f8cb1047a94d0d607a767957c589f2
kwn <kowilliam03@gmail.com>
2022-03-26 15:46:07 +0800

    modify some package's config

--- config/init-package.el
+++ config/init-package.el
@@ -40,28 +40,9 @@ locate PACKAGE."
 
 (package-initialize)
 
-;; (autoload 'magit-submodule-add "magit" "" t)
-;; (autoload 'magit-submodule-remove "magit" "" t)
-
-;; (defun kwn/compile-package ()
-;;   (interactive)
-;;   (native-compile-async "~/.emacs.d/packages" 'recursively))
-
-;; (defun kwn/install-package ()
-;;   (interactive)
-;;   (setq url (read-string "Git Repository's URL: "))
-;;   (let ((pkg-dir "packages/"))
-;;     (magit-submodule-add
-;;      url
-;;      (concat pkg-dir (file-name-base url))
-;;      (file-name-base url)
-;;      ))
-;;   )
-
-;; (defun kwn/remove-package ()
-;;   (interactive)
-;;   (magit-submodule-remove (list (magit-read-module-path "Remove module")) "--force" nil)
-;;   )
+(defun kwn/compile-package ()
+  (interactive)
+  (native-compile-async "~/.emacs.d/elpa" 'recursively))
 
 
 (provide 'init-package)
